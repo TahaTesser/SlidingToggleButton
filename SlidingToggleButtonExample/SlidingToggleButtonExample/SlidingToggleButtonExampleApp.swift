a64f182af9896ffac9bc374e4d4f200deb424147
Taha Tesser <tessertaha@gmail.com>
2024-12-25 21:22:14 +0200

    feat: initial commit

---
+++ SlidingToggleButtonExample/SlidingToggleButtonExample/SlidingToggleButtonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingToggleButtonExampleApp.swift
+//  SlidingToggleButtonExample
+//
+//  Created by Taha Tesser on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SlidingToggleButtonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
